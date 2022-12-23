ef5a60d5f4f0387ab302a26340ac6cb1afd22777
paytonwu90 <sw9008k@gmail.com>
2022-12-24 01:58:46 +0800

    Add Swiper

--- js/all.js
+++ js/all.js
@@ -18,3 +18,38 @@ function addListeners() {
     });
   });
 }
+
+function goTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  spaceBetween: 16,
+  slidesPerView: 1,
+  loop: true,
+  /* autoplay: {
+    delay: 5000,
+  }, */
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    },
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  
+  // Navigation arrows
+  /*navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },*/
+  
+});
